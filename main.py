a4de17b9d3cf0494b74dbb72d0f6642a2057b8b2
28subedib <28subedib@bwschools.net>
2025-03-07 08:46:21 -0500

    commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import turtle
+
+screen = turtle.Screen()
+screen.screensize(500,500)
+screen.bgcolor('Tan')
+t = turtle.Turtle()
+
+#circle
+t.penup()
+t.goto(-80,15)
+t.pendown()
+t.pensize(1)
+t.pencolor('blue')
+t.circle(35)
+#circle
+t.penup()
+t.goto(0,15)
+t.pendown()
+t.pencolor('black')
+t.circle(35)
+#circle
+t.penup()
+t.goto(80,15)
+t.pendown()
+t.pencolor('red')
+t.circle(35)
+#circle
+t.penup()
+t.goto(-40,-15)
+t.pendown()
+t.pencolor('yellow')
+t.circle(35)
+#circle
+t.penup()
+t.goto(40,-15)
+t.pendown()
+t.pencolor('green')
+t.circle(35)
+#text on the screen
+t.penup()
+t.goto(-150,100)
+t.pendown()
+t.pencolor("purple")
+t.write("Winter Olympics", font=("Arial",30,"bold"))
+#text on the screen
+t.penup()
+t.goto(-40,-100)
+t.pendown()
+t.pencolor("purple")
+t.write("2026", font=("Arial",30,"bold"))
+#This is the last line of code
+turtle.done()
+
+
+
+
+
+
